3ac94e68867c9a31d92500bc14a67f93cdb1a6f0
Gabriel Ralusi <gabriel.cvramos@gmail.com>
2023-09-07 05:35:13 +0000

    First setup with Github Codespace

---
+++ app/ralusi/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function RalusiPage() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-semibold text-gray-300">Gabriel Ralusi</h1>
+        </div>
+    )
+}
